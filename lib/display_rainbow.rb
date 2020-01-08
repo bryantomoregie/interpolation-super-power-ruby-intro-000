2f8b49b923b7fdd8f279bd47deef2c304708ed65
Bryant Omoregie <unclebryant@gmail.com>
2020-01-08 18:55:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+def display_rainbow
+  puts "R:#{display_rainbow[0]}, #{display_rainbow[1]}, #{display_rainbow[2]}, #{display_rainbow[3]}, #{display_rainbow[4]}, #{display_rainbow[5]}, #{display_rainbow[6]}"# Write your #display_rainbow method here
+end
